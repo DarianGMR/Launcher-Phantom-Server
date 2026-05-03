009bc50e31054b090b5aab346faf5fd5ecf9cd55
DarianGMR <167760447+DarianGMR@users.noreply.github.com>
2026-05-03 09:22:11 -0400

    Add files via upload

--- Models/VersionInfo.cs
+++ Models/VersionInfo.cs
@@ -3,7 +3,7 @@ namespace LauncherPhantomServer.Models
     public class VersionInfo
     {
         public string Version { get; set; } = "0.1.0";
-        public string DownloadUrl { get; set; } = "http://0.0.0.0:5000/downloads/launcher-update.exe";
+        public string DownloadUrl { get; set; } = "http://0.0.0.0:5000/update/LauncherPhantom.exe";
         public string Changes { get; set; } = "- Version inicial";
         public bool Required { get; set; } = false;
     }
